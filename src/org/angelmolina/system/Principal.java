4c9f54804d68b1938ecd0f2f77079d00bc0253fc
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-06-04 11:31:11 -0600

    000 - Primer commit configuracion basica y .gitignore

---
+++ src/org/angelmolina/system/Principal.java
@@ -0,0 +1,15 @@
+
+package org.angelmolina.system;
+
+/**
+ *
+ * @author informatica
+ */
+public class Principal {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
