5742567306584a33d406e026eb47f7cb4c2cc002
Herberth Cuyuch <hcuyuch-2026046@kinal.edu.gt>
2026-06-05 10:44:45 -0600

    010 Se agregaron cambios a Principal

--- src/org/angelmolina/system/Principal.java
+++ src/org/angelmolina/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
   
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - Soy Herberth");
+        System.out.println("Estoy en kinal - 2026046");
                 
      
     }
