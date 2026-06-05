610408089956d1462dae7a587f18040bc6a614ee
Angel Molina <amolina-2026053@kinal.edu.gt>
2026-06-05 10:45:23 -0600

    009 se actualizo la clase principal, agregue un saludo

--- src/org/angelmolina/system/Principal.java
+++ src/org/angelmolina/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
   
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - soy Angel");
+        System.out.println("Estoy en kinal - 2026053");
                 
      
     }
